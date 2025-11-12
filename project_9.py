6f2f9cc711a19a2af884d0663ca3be325d217a1c
Joshitha37 <joshitha.samineni@gmail.com>
2025-11-12 15:15:11 -0500

    Update project_9.py

--- project_9.py
+++ project_9.py
@@ -1,1 +1,24 @@
-# adding the project code
+import os
+from library import functions
+from library.classes_9 import Budget 
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+name = input("Enter your name: ")
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print(f"Hey {name}, this is BudgetBuddy! Your personal Budgeting Assistant.\n")
+income = float(input("Enter your monthly income (only numbers): "))
+
+grocery = Budget("Grocery", "Milk")
+car = Budget("car", "Gas")
+
+grocery.add_expenses()
+car.add_expenses()
+
+total_expenses = []
+total_expenses.append(grocery.get_expenses())
+total_expenses.append(car.get_expenses())
+
+bal = functions.calc_balance(income, sum(total_expenses))
+functions.financial_status(bal)
